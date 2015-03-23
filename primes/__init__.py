be9ad143ba0529eddd161be9f5d0c69dbfd4ecdf
Chris Calloway <cbc@chriscalloway.org>
2015-03-23 17:55:42 -0400

    Initial package contents.

---
+++ primes/__init__.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+
+"""
+Package of prime number objects.
+"""
+
+
+def prime_generator_function():
+    """Return a generator for prime numbers."""
+
+    p = [2, ]
+    while True:
+        yield p[-1]
+        q = p[-1] + 1
+        while True:
+            for x in p:
+                if not (q % x):
+                    break
+            else:
+                p.append(q)
+                break
+            q += 1
+
+
+pgf = prime_generator_function
+
+
+def primes(n):
+    """Return a sequence of the first n primes."""
+
+    pg = pgf()
+    return [pg.next() for x in range(n)]
+
+
+def prime(n):
+    """Return the nth prime."""
+
+    return primes(n)[-1]
---
+++ primes/__main__.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+
+"""
+A __main__ namespace for the primes package.
+"""
+
+import sys
+import argparse
+from time import time
+from primes import primes
+
+
+def main(argv):
+    """Call primes when the package is run as a script."""
+
+    parser = argparse.ArgumentParser(
+    	prog='prime',
+        description='Display the first N primes.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('number',
+                        metavar='N',
+                        nargs='?',
+                        default='100',
+                        help='Number of primes to generate')
+    parser.add_argument('-t',
+                        action='store_true',
+                        help='Display elapsed time')
+    args = parser.parse_args(argv)
+
+    try:
+        number = int(args.number, 0)
+        if args.t:
+            start = time()
+        print " ".join([str(n) for n in primes((number))])
+        if args.t:
+            print
+            print "Elapsed time is {} seconds.".format(time() - start)
+    except Exception as e:
+        parser.print_help()
+        print
+        print e
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
