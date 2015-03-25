f9a638fa35d4a8e93585f2c3a5a14df468a42157
Chris Calloway <cbc@chriscalloway.org>
2015-03-25 12:10:36 -0400

    Release testing.

--- setup.py
+++ setup.py
@@ -5,20 +5,24 @@
 from setuptools import setup
 from setuptools import find_packages
 
+__version__ = "1.0.1"
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+def long_description():
+    with open("README.rst") as r:
+        l1 = r.read()
+    with open("HISTORY.txt") as h:
+        l2 = h.read()
+    return "\n".join([l1, l2, ])
 
 
-setup(name='primer',
-      version='1.0',
+setup(name="primer",
+      version=__version__,
       license="GPL2",
       packages=find_packages(),
-      author='Chris Calloway',
-      author_email='cbc@chriscalloway.org',
-      description='A primer for prime numbers',
-      long_description=readme(),
+      author="Chris Calloway",
+      author_email="cbc@chriscalloway.org",
+      description="A primer for prime numbers",
+      long_description=long_description(),
       url="https://github.com/cbcunc/primer",
       keywords="Prime number primes primordial",
       classifiers=["Development Status :: 5 - Production/Stable",
